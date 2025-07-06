fd5cce7c4d0b800f29a49f68cc0acfd63e0107ef
MarceloSavian <marcelo.savian@gmail.com>
2025-07-06 23:09:32 +0200

    feat: improve config per environment

--- infra/api.ts
+++ infra/api.ts
@@ -2,11 +2,13 @@ import { loadConfig } from './config';
 import { personTable } from './database';
 import { personCreatedBus } from './eventbridge';
 
+const config = loadConfig();
+
 const api = new sst.aws.ApiGatewayV2('PersonApi', {
   domain: {
-    name: 'api.marcelosavian.com',
+    name: config.domain,
     path: 'v1',
-    cert: 'arn:aws:acm:eu-west-3:435238036861:certificate/0232bec4-430e-473d-95ca-c7f688305a1e',
+    cert: config.certificateArn,
   },
   link: [personTable, personCreatedBus],
 });
--- infra/config.ts
+++ infra/config.ts
@@ -1,4 +1,3 @@
-import { Resource } from 'sst';
 import zod from 'zod';
 
 const Env = zod.enum(['test', 'prod']);
@@ -11,18 +10,18 @@ export const AppConfig = zod.object({
 export type AppConfig = zod.infer<typeof AppConfig>;
 
 export const loadConfig = (): AppConfig => {
-  const env = Env.catch('test').parse(Resource.App.stage);
+  const env = Env.catch('test').parse($app.stage);
 
   const domain =
-    Resource.App.stage == env
+    $app.stage == env
       ? `api.${env}.marcelosavian.com`
-      : `api.${env}.${Resource.App.stage}.marcelosavian.com`;
+      : `${$app.stage}.api.${env}.marcelosavian.com`;
 
   const config: Record<Env, AppConfig> = {
     test: {
       domain,
       certificateArn:
-        'arn:aws:acm:eu-west-3:435238036861:certificate/0232bec4-430e-473d-95ca-c7f688305a1e',
+        'arn:aws:acm:eu-west-3:435238036861:certificate/b00857c0-9555-43a8-a86b-6dbe743e4bf1',
     },
     prod: {
       domain,
