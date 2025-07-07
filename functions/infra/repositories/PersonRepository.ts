d750a8ced329bf862d92be212d9d03b14b03c35e
MarceloSavian <marcelo.savian@gmail.com>
2025-07-07 13:42:18 +0200

    fix: remove console

--- functions/infra/repositories/PersonRepository.ts
+++ functions/infra/repositories/PersonRepository.ts
@@ -28,7 +28,6 @@ export class PersonRepository implements IPersonRepository {
   }
 
   async get(id: NonEmptyString): Promise<Person | null> {
-    console.log(id);
     const command = new GetCommand({
       TableName: Resource.Person.name,
       Key: {
